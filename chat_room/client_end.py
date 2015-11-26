fa10752d897f1cd1480e2aa8ae586026bb559937
Sorosliu1029 <soros.liu1029@gmail.com>
2015-11-26 11:09:43 +0800

    a simple chat room with GUI implement

---
+++ chat_room/client_end.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+# encoding:utf-8
+import telnetlib
+import wx
+import thread
+from time import sleep
+__author__ = 'liuyang'
+
+
+# login frame GUI
+class LoginFrame(wx.Frame):
+    """
+    login window
+    """
+    def __init__(self, parent, id, title, size):
+        """initialize, add widgets and bind to evert"""
+        wx.Frame.__init__(self, parent, id, title)
+        self.SetSize(size)
+        self.Center()
+        self.serverAddressLabel = wx.StaticText(self, label='Server Address',
+                                                pos=(10, 50), size=(120, 50))
+        self.userNameLabel = wx.StaticText(self, label='User Name',
+                                           pos=(40, 100), size=(120,25))
+        self.serverAddress = wx.TextCtrl(self, pos=(120, 47), size=(150,25))
+        self.userName = wx.TextCtrl(self, pos=(120, 97), size=(150,25))
+        self.loginButton = wx.Button(self, label='Login', pos=(80,145),size=(130,30))
+        self.loginButton.Bind(wx.EVT_BUTTON, self.login)
+        self.Show()
+
+    def login(self, event):
+        """process the login"""
+        try:
+            serverAddress = self.serverAddress.GetLineText(0).split(':')
+            con.open(serverAddress[0], port=int(serverAddress[1]), timeout=10)
+            response = con.read_some()
+            if response != 'Connect Success':
+                self.showDialog('Error', 'Connect Fail!', (95,20))
+                return
+            con.write('login' + str(self.userName.GetLineText(0)) + '\n')
+            response = con.read_some()
+            if response == 'User Name Empty!':
+                self.showDialog('Error', 'User Name Empty!', (135,20))
+            elif response == 'User Name Exist!':
+                self.showDialog('Error', 'User Name Exist!', (135, 20))
+            else:
+                self.Close()
+                ChatFrame(None, -2, title='Python Chat Client', size=(500, 350))
+        except Exception:
+            self.showDialog('Error', 'Connect Fail!', (95,20))
+
+    def showDialog(self, title, content, size):
+        """show the error info"""
+        dialog = wx.Dialog(self, title=title, size=size)
+        dialog.Center()
+        wx.StaticText(dialog, label=content)
+        dialog.ShowModal()
+
+# chat window
+class ChatFrame(wx.Frame):
+    """
+    chatting window
+    """
+    def __init__(self, parent, id, title, size):
+        wx.Frame.__init__(self, parent, id, title)
+        self.SetSize(size)
+        self.Center()
+        self.chatFrame = wx.TextCtrl(self, pos=(5,5), size=(490,310),
+                                     style=wx.TE_MULTILINE | wx.TE_READONLY)
+        self.message = wx.TextCtrl(self, pos=(5,320), size=(300,25))
+        self.sendButton = wx.Button(self, label='Send', pos=(310,320), size=(58,25))
+        self.usersButton = wx.Button(self, label='Users', pos=(373,320), size=(58,25))
+        self.closeButton = wx.Button(self, label='Close', pos=(436,320), size=(58,25))
+        self.sendButton.Bind(wx.EVT_BUTTON, self.send)
+        self.usersButton.Bind(wx.EVT_BUTTON, self.lookUsers)
+        self.closeButton.Bind(wx.EVT_BUTTON, self.close)
+        thread.start_new_thread(self.receive, ())
+        self.Show()
+
+    def send(self, event):
+        """send message"""
+        message = str(self.message.GetLineText(0)).strip()
+        if message != '':
+            con.write('say ' + message + '\n')
+            self.message.Clear()
+
+    def lookUsers(self, event):
+        """look for the online users"""
+        con.write('look\n')
+
+    def close(self, event):
+        """close the window"""
+        con.write('logout\n')
+        con.close()
+        self.Close()
+
+    def receive(self):
+        """receive message from the server"""
+        while True:
+            sleep(0.6)
+            result = con.read_very_eager()
+            if result != '':
+                self.chatFrame.AppendText(result)
+
+
+if __name__ == '__main__':
+    app = wx.App()
+    con = telnetlib.Telnet()
+    LoginFrame(None, -1, title='Login', size=(280,200))
+    app.MainLoop()
---
+++ chat_room/server_end.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python
+from asyncore import dispatcher
+from asynchat import async_chat
+import socket, asyncore
+__author__ = 'liuyang'
+
+
+# server class
+class ChatServer(dispatcher):
+    """
+    chatting server
+    """
+    def __init__(self, port):
+        dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind(('', port))
+        self.listen(5)
+        self.users = {}
+        self.main_room = ChatRoom(self)
+
+    def handle_accept(self):
+        conn, addr = self.accept()
+        ChatSession(self, conn)
+
+
+# session class
+class ChatSession(async_chat):
+    """
+    responsible for communicating with a single user
+    """
+    def __init__(self, server, sock):
+        async_chat.__init__(self, sock)
+        self.server = server
+        self.set_terminator('\n')
+        self.data = []
+        self.name = None
+        self.enter(LoginRoom(server))
+
+    def enter(self, room):
+        """remove self from current rooms, then add to the point room"""
+        try:
+            cur = self.room
+        except AttributeError:
+            pass
+        else:
+            cur.remove(self)
+        self.room = room
+        room.add(self)
+
+    def collect_incoming_data(self, data):
+        """receive data from client"""
+        self.data.append(data)
+
+    def found_terminator(self):
+        """when one piece of data from client has been ended"""
+        line = ''.join(self.data)
+        self.data = []
+        try:
+            self.room.handle(self, line)
+        except EndSession:
+            self.handle_close()
+
+    def handle_close(self):
+        async_chat.handle_close(self)
+        self.enter(LogoutRoom(self.server))
+
+# command interpreter class
+class CommandHandler:
+    """
+    to handle the command, e.g. login, query for online users, send message
+    """
+    def unknown(self, session, cmd):
+        """response for unknown command"""
+        session.push('Unknown command: %s\n' % cmd)
+
+    def handle(self, session, line):
+        """handle the command"""
+        if not line.strip():
+            return
+        parts = line.split(' ',1)
+        cmd = parts[0]
+        try:
+            line = parts[1].strip()
+        except IndexError:
+            line = ''
+        meth = getattr(self, 'do_' + cmd, None)
+        try:
+            meth(session, line)
+        except TypeError:
+            self.unknown(session, cmd)
+
+
+# room class (abstract class)
+class Room(CommandHandler):
+    """
+    including environment for multiple user.
+    responsible for basic command handle and broadcast
+    """
+    def __init__(self, server):
+        self.server = server
+        self.sessions = []
+
+    def add(self, session):
+        """an user enter the room"""
+        self.sessions.append(session)
+
+    def remove(self, session):
+        """an user leave the room"""
+        self.sessions.remove(session)
+
+    def broadcast(self, line):
+        """broadcast a certain message to all users"""
+        for session in self.sessions:
+            session.push(line)
+
+    def do_logout(self, session, line):
+        """leave the room"""
+        raise EndSession
+
+
+# EndSession error class
+class EndSession(Exception):
+    """self define the exception when the session ended"""
+    pass
+
+
+class LoginRoom(Room):
+    """
+    room for user who has just login
+    """
+    def add(self, session):
+        """response when user connect successful"""
+        Room.add(self, session)
+        session.push('Connect Success')
+
+    def login(self, session, line):
+        """handle the login command"""
+        name = line.strip()
+        if not name:
+            session.push('User Name Empty!')
+        elif name in self.server.users:
+            session.push('User Name Exist!')
+        else:
+            session.name = name
+            session.enter(self.server.main_room)
+
+
+class ChatRoom(Room):
+    """
+    room for chatting
+    """
+    def add(self, session):
+        """broadcast that there is a new user entering"""
+        session.push('Login Success')
+        self.broadcast(session.name + ' has entered the room.\n')
+        self.server.users[session.name] = session
+        Room.add(self, session)
+
+    def remove(self, session):
+        """broadcast that there is someone leaved"""
+        Room.remove(self, session)
+        self.broadcast(session.name + ' has left the room.\n')
+
+    def do_say(self, session, line):
+        """client send message"""
+        self.broadcast(session.name + ' : ' + line + '\n')
+
+    def do_look(self, session, line):
+        """query for the online users"""
+        session.push('Online Users:\n')
+        for other in self.sessions:
+            session.push(other.name + '\n')
+
+
+class LogoutRoom(Room):
+    """
+    room for user who will leave
+    """
+    def add(self, session):
+        """remove from the server"""
+        try:
+            del self.server.users[session.name]
+        except KeyError:
+            pass
+
+PORT = 8888
+if __name__ == '__main__':
+    s = ChatServer(PORT)
+    try:
+        asyncore.loop()
+    except KeyboardInterrupt:
+        print()
